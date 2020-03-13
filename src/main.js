e5cf0c59b8ab3730f719f3c59eedcff3b002df5c
Margot <maria16ruiz81@gmail.com>
2020-03-12 19:30:14 -0600

    prueba

--- src/main.js
+++ src/main.js
@@ -2,3 +2,10 @@ import { example } from './data.js';
 
 console.log(example);
 
+let refreshButton = document.getElementById('reload');
+
+function refreshPage() {
+  window.location.reload();
+}
+
+refreshButton.addEventListener('click', refreshPage);
